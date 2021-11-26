10627af1ec9f2e9535cbab39ac9f89449d0df8e0
Barry McGettigan <BMcGettigan@zarion.com>
2021-11-26 12:26:55 +0000

    Commit my changes for workshop

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,11 +27,19 @@ const Header = () => {
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
+  padding: 18px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  align-items: baseline;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  margin-left: 40px;
+  gap: 32px;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -29,21 +29,37 @@ const ShoeCard = ({
     ? 'on-sale'
     : isNewShoe(releaseDate)
       ? 'new-release'
-      : 'default'
+      : 'default';
+  
+  if (variant)
+
+   
 
   return (
     <Link href={`/shoe/${slug}`}>
       <Wrapper>
         <ImageWrapper>
           <Image alt="" src={imageSrc} />
+          {variant === 'on-sale' ? (<SaleFlag>On Sale</SaleFlag>) : undefined}
+          {variant === 'new-release' ? (<ReleaseFlag>Just Released!</ReleaseFlag>) : undefined}
         </ImageWrapper>
         <Spacer size={12} />
         <Row>
           <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
+          <Price
+            style={{
+              '--color':
+              variant === 'on-sale' ? COLORS.gray[700] : undefined,
+              '--text-decoration':
+              variant === 'on-sale' ? 'line-through' : undefined
+            }}
+          >{formatPrice(price)}</Price>
         </Row>
         <Row>
           <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
+         {variant === 'on-sale' ? (
+           <SalePrice>{formatPrice(salePrice)}</SalePrice>
+         ) : undefined}
         </Row>
       </Wrapper>
     </Link>
@@ -61,10 +77,14 @@ const ImageWrapper = styled.div`
   position: relative;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+  width: 100%;
+`;
 
 const Row = styled.div`
   font-size: 1rem;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Name = styled.h3`
@@ -72,7 +92,10 @@ const Name = styled.h3`
   color: ${COLORS.gray[900]};
 `;
 
-const Price = styled.span``;
+const Price = styled.span`
+  color: var(--color);
+  text-decoration: var(--text-decoration);
+`;
 
 const ColorInfo = styled.p`
   color: ${COLORS.gray[700]};
@@ -83,4 +106,23 @@ const SalePrice = styled.span`
   color: ${COLORS.primary};
 `;
 
+const Flag = styled.div`
+  position: absolute;
+  top: 12px;
+  right: -4px;
+  background-color: red;
+  height: 32px;
+  padding: 0 10px;
+  font-weight: ${WEIGHTS.bold};
+  line-height: 32px;
+  color: white;
+`
+
+const SaleFlag = styled(Flag)`
+  background-color: ${COLORS.primary};
+`
+
+const ReleaseFlag = styled(Flag)`
+  background-color: ${COLORS.secondary};
+`
 export default ShoeCard;
